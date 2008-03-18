fd48f408e4ea9275aabfc8d378787d3ff09b9611
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-18 19:14:46 +0000

    gwt 1.5m1 + gwt-maven + gwt-log

--- emite/src/main/java/com/calclab/examplechat/client/ChatExampleEntryPoint.java
+++ emite/src/main/java/com/calclab/examplechat/client/ChatExampleEntryPoint.java
@@ -2,6 +2,7 @@ package com.calclab.examplechat.client;
 
 import java.util.List;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.calclab.emite.client.Xmpp;
 import com.calclab.emite.client.bosh.BoshOptions;
 import com.calclab.emite.client.log.LoggerOutput;
@@ -12,6 +13,8 @@ import com.calclab.emite.client.x.im.roster.RosterListener;
 import com.calclab.emite.client.x.im.session.SessionListener;
 import com.calclab.emite.client.x.im.session.Session.State;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -26,157 +29,180 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ChatExampleEntryPoint implements EntryPoint {
-	private Button btnLogin;
-	private Button btnLogout;
-	private TextBox messageIn;
-	private TextArea messageOutput;
-	private TextArea out;
-	private PasswordTextBox passwordInput;
-	private TextBox toIn;
-	private TextBox userNameInput;
-	private ListBox userSelector;
-	private Xmpp xmpp;
+    private Button btnLogin;
+    private Button btnLogout;
+    private TextBox messageIn;
+    private TextArea messageOutput;
+    // private TextArea out;
+    private PasswordTextBox passwordInput;
+    private TextBox toIn;
+    private TextBox userNameInput;
+    private ListBox userSelector;
+    private Xmpp xmpp;
 
-	public void onModuleLoad() {
-		this.out = new TextArea();
-		createInterface();
+    public void onModuleLoad() {
+        /*
+         * Install an UncaughtExceptionHandler which will produce <code>FATAL</code>
+         * log messages
+         */
+        Log.setUncaughtExceptionHandler();
 
-		this.xmpp = Xmpp.create(new BoshOptions("http-bind", "localhost"), new LoggerOutput() {
-			public void log(final int level, final String message) {
-				print(message);
-			}
-		});
+        // At the moment, in runtime:
+        Log.setCurrentLogLevel(Log.LOG_LEVEL_DEBUG);
 
-		xmpp.addSessionListener(new SessionListener() {
-			public void onStateChanged(final State old, final State current) {
-				print("STATE CHANGED: " + current + " - old: " + old);
-				switch (current) {
-				case connected:
-					btnLogin.setEnabled(false);
-					btnLogout.setEnabled(true);
-				case connecting:
-					btnLogin.setEnabled(false);
-				case disconnected:
-					btnLogin.setEnabled(true);
-					btnLogout.setEnabled(false);
-				}
-			}
-		});
+        /*
+         * Use a deferred command so that the UncaughtExceptionHandler catches
+         * any exceptions in onModuleLoadCont()
+         */
+        DeferredCommand.addCommand(new Command() {
+            public void execute() {
+                onModuleLoadCont();
+            }
+        });
+    }
 
-		xmpp.getRoster().addListener(new RosterListener() {
-			public void onRosterChanged(final List<RosterItem> roster) {
-				for (final RosterItem item : roster) {
-					userSelector.addItem(item.getName(), item.getJid());
-				}
-			}
-		});
+    public void onModuleLoadCont() {
+        // this.out = new TextArea();
+        createInterface();
 
-		xmpp.addMessageListener(new MessageListener() {
-			public void onReceived(final Message message) {
-				String text = messageOutput.getText();
-				text += "\nIN [" + message.getFrom() + "]\n";
-				text += message.getBody();
-				messageOutput.setText(text);
-			}
-		});
+        this.xmpp = Xmpp.create(new BoshOptions("http-bind", "localhost"), new LoggerOutput() {
+            public void log(final int level, final String message) {
+                print(message);
+            }
+        });
 
-	}
+        xmpp.addSessionListener(new SessionListener() {
+            public void onStateChanged(final State old, final State current) {
+                print("STATE CHANGED: " + current + " - old: " + old);
+                switch (current) {
+                case connected:
+                    btnLogin.setEnabled(false);
+                    btnLogout.setEnabled(true);
+                case connecting:
+                    btnLogin.setEnabled(false);
+                case disconnected:
+                    btnLogin.setEnabled(true);
+                    btnLogout.setEnabled(false);
+                }
+            }
+        });
 
-	public void print(final String text) {
-		out.setText(out.getText() + text + "\n");
-	}
+        xmpp.getRoster().addListener(new RosterListener() {
+            public void onRosterChanged(final List<RosterItem> roster) {
+                for (final RosterItem item : roster) {
+                    userSelector.addItem(item.getName(), item.getJid());
+                }
+            }
+        });
 
-	private HorizontalPanel createButtonsPane() {
-		final HorizontalPanel buttons = new HorizontalPanel();
-		btnLogin = new Button("Login", new ClickListener() {
-			public void onClick(final Widget source) {
-				xmpp.login(userNameInput.getText(), passwordInput.getText());
-				btnLogin.setEnabled(false);
-				btnLogout.setEnabled(true);
-			}
-		});
-		buttons.add(btnLogin);
-		btnLogout = new Button("Logout", new ClickListener() {
-			public void onClick(final Widget arg0) {
-				xmpp.logout();
-				btnLogout.setEnabled(true);
-				btnLogin.setEnabled(true);
-			}
-		});
-		buttons.add(btnLogout);
+        xmpp.addMessageListener(new MessageListener() {
+            public void onReceived(final Message message) {
+                String text = messageOutput.getText();
+                text += "\nIN [" + message.getFrom() + "]\n";
+                text += message.getBody();
+                messageOutput.setText(text);
+            }
+        });
 
-		buttons.add(new Button("Clear", new ClickListener() {
-			public void onClick(final Widget source) {
-				out.setText("");
-			}
-		}));
-		return buttons;
-	}
+    }
 
-	private void createInterface() {
-		final VerticalPanel vertical = new VerticalPanel();
-		vertical.add(createButtonsPane());
-		vertical.add(createLoginPane());
-		vertical.add(createMessagePane());
-		vertical.add(createOutputPane());
+    public void print(final String text) {
+        Log.debug(text);
+        // out.setText(out.getText() + text + "\n");
+    }
 
-		RootPanel.get().add(vertical);
-	}
+    private HorizontalPanel createButtonsPane() {
+        final HorizontalPanel buttons = new HorizontalPanel();
+        btnLogin = new Button("Login", new ClickListener() {
+            public void onClick(final Widget source) {
+                xmpp.login(userNameInput.getText(), passwordInput.getText());
+                btnLogin.setEnabled(false);
+                btnLogout.setEnabled(true);
+            }
+        });
+        buttons.add(btnLogin);
+        btnLogout = new Button("Logout", new ClickListener() {
+            public void onClick(final Widget arg0) {
+                xmpp.logout();
+                btnLogout.setEnabled(true);
+                btnLogin.setEnabled(true);
+            }
+        });
+        buttons.add(btnLogout);
 
-	private HorizontalPanel createLoginPane() {
-		final HorizontalPanel login = new HorizontalPanel();
-		userNameInput = new TextBox();
-		passwordInput = new PasswordTextBox();
-		login.add(new Label("user name:"));
-		login.add(userNameInput);
-		login.add(new Label("password"));
-		login.add(passwordInput);
-		return login;
-	}
+        buttons.add(new Button("Clear", new ClickListener() {
+            public void onClick(final Widget source) {
+                Log.clear();
+                // out.setText("");
+            }
+        }));
+        return buttons;
+    }
 
-	private VerticalPanel createMessagePane() {
-		final VerticalPanel pane = new VerticalPanel();
+    private void createInterface() {
+        final VerticalPanel vertical = new VerticalPanel();
+        vertical.add(createButtonsPane());
+        vertical.add(createLoginPane());
+        vertical.add(createMessagePane());
+        vertical.add(createOutputPane());
 
-		final HorizontalPanel controls = new HorizontalPanel();
-		pane.add(controls);
+        RootPanel.get().add(vertical);
+    }
 
-		toIn = new TextBox();
-		controls.add(toIn);
-		messageIn = new TextBox();
-		controls.add(messageIn);
-		final Button btnSend = new Button("send", new ClickListener() {
-			public void onClick(Widget arg0) {
-				String msg = messageIn.getText();
-				messageIn.setText("");
-				messageOutput.setText(messageOutput.getText() + "\n" + "sending: " + msg);
-				xmpp.send(toIn.getText(), msg);
-			}
-		});
-		controls.add(btnSend);
+    private HorizontalPanel createLoginPane() {
+        final HorizontalPanel login = new HorizontalPanel();
+        userNameInput = new TextBox();
+        passwordInput = new PasswordTextBox();
+        login.add(new Label("user name:"));
+        login.add(userNameInput);
+        login.add(new Label("password"));
+        login.add(passwordInput);
+        return login;
+    }
 
-		final HorizontalPanel split = new HorizontalPanel();
-		userSelector = new ListBox(true);
-		userSelector.addClickListener(new ClickListener() {
-			public void onClick(final Widget arg0) {
-				final String jid = userSelector.getValue(userSelector.getSelectedIndex());
-				toIn.setText(jid);
-			}
-		});
-		split.add(userSelector);
-		final TabPanel chatTabs = new TabPanel();
-		split.add(chatTabs);
-		messageOutput = new TextArea();
-		split.add(messageOutput);
+    private VerticalPanel createMessagePane() {
+        final VerticalPanel pane = new VerticalPanel();
 
-		pane.add(split);
+        final HorizontalPanel controls = new HorizontalPanel();
+        pane.add(controls);
 
-		return pane;
-	}
+        toIn = new TextBox();
+        controls.add(toIn);
+        messageIn = new TextBox();
+        controls.add(messageIn);
+        final Button btnSend = new Button("send", new ClickListener() {
+            public void onClick(Widget arg0) {
+                String msg = messageIn.getText();
+                messageIn.setText("");
+                messageOutput.setText(messageOutput.getText() + "\n" + "sending: " + msg);
+                xmpp.send(toIn.getText(), msg);
+            }
+        });
+        controls.add(btnSend);
 
-	private HorizontalPanel createOutputPane() {
-		final HorizontalPanel split = new HorizontalPanel();
-		split.add(out);
-		return split;
-	}
+        final HorizontalPanel split = new HorizontalPanel();
+        userSelector = new ListBox(true);
+        userSelector.addClickListener(new ClickListener() {
+            public void onClick(final Widget arg0) {
+                final String jid = userSelector.getValue(userSelector.getSelectedIndex());
+                toIn.setText(jid);
+            }
+        });
+        split.add(userSelector);
+        final TabPanel chatTabs = new TabPanel();
+        split.add(chatTabs);
+        messageOutput = new TextArea();
+        split.add(messageOutput);
+
+        pane.add(split);
+
+        return pane;
+    }
+
+    private HorizontalPanel createOutputPane() {
+        final HorizontalPanel split = new HorizontalPanel();
+        // split.add(out);
+        return split;
+    }
 
 }
