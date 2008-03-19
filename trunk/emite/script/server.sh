711ad022cf0a87855b43857896a0e91ed26d4d5a
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-19 00:06:18 +0000

    Sample UI changes

--- trunk/emite/script/server.sh
+++ trunk/emite/script/server.sh
@@ -4,6 +4,5 @@ if [ -d src/main/webapp/gwt/com.calclab.examplechat.Example ]
 then
   rm -rf src/main/webapp/gwt/com.calclab.examplechat.Example/
 fi
-#cp -a target/emite-0.0.1/gwt/com.calclab.examplechat.Example src/main/webapp/gwt/
 cp -a target/emite-0.0.1/com.calclab.examplechat.Example/ src/main/webapp/gwt/
 mvn jetty:run
