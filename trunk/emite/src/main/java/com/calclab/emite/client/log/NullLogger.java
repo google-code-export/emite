7e1aec34be199e52ec18857512c1c44cd089468d
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-23 03:44:54 +0000

    More log levels

--- trunk/emite/src/main/java/com/calclab/emite/client/log/NullLogger.java
+++ trunk/emite/src/main/java/com/calclab/emite/client/log/NullLogger.java
@@ -2,14 +2,21 @@ package com.calclab.emite.client.log;
 
 public class NullLogger implements Logger {
 
-	public final void debug(final String pattern, final Object... params) {
-	}
+    public final void debug(final String pattern, final Object... params) {
+    }
 
-	public final void info(final String pattern, final Object... params) {
+    public final void info(final String pattern, final Object... params) {
+    }
 
-	}
+    public final void log(final int level, final String pattern, final Object... params) {
+    }
 
-	public final void log(final int level, final String pattern, final Object... params) {
-	}
+    public void error(final String pattern, final Object... params) {
+    }
 
+    public void fatal(final String pattern, final Object... params) {
+    }
+
+    public void warn(final String pattern, final Object... params) {
+    }
 }
