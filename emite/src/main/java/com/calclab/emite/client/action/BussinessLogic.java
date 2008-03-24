5fb080114e545fcec8c781bbf930ffa0e335084e
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-24 12:08:55 +0000

    dispatcher refactoring part 3

--- emite/src/main/java/com/calclab/emite/client/action/BussinessLogic.java
+++
@@ -1,7 +0,0 @@
-package com.calclab.emite.client.action;
-
-import com.calclab.emite.client.packet.Packet;
-
-public interface BussinessLogic {
-	public abstract Packet logic(final Packet received);
-}
