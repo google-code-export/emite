b1d090099f7c8fa4cd1076158b47d527b27d00c6
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-24 21:21:52 +0000

    Some more icons

--- trunk/emite/src/main/java/com/calclab/examplechat/client/chatuiplugin/utils/MultiChatSamples.java
+++ trunk/emite/src/main/java/com/calclab/examplechat/client/chatuiplugin/utils/MultiChatSamples.java
@@ -1,5 +1,6 @@
 package com.calclab.examplechat.client.chatuiplugin.utils;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.calclab.examplechat.client.chatuiplugin.dialog.MultiChat;
 import com.calclab.examplechat.client.chatuiplugin.groupchat.GroupChat;
 import com.calclab.examplechat.client.chatuiplugin.groupchat.GroupChatUser;
@@ -8,25 +9,64 @@ import com.calclab.examplechat.client.chatuiplugin.pairchat.PairChatUser;
 
 public class MultiChatSamples {
     public static void show(final MultiChat extChatDialog) {
+        Log.debug("Adding pair chats");
         PairChatUser pairUser = new PairChatUser("Mark", "ma");
         PairChat pairChat = extChatDialog.createPairChat(pairUser);
+        pairChat.addMessage("ma", "foo");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
+        pairChat.addMessage("ma", "testing");
         pairChat.addMessage("ma", "testing");
 
+        PairChatUser pairUser2 = new PairChatUser("Fran", "frank");
+        PairChat pairChat2 = extChatDialog.createPairChat(pairUser2);
+        pairChat2.addMessage("frank", "foo");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+        pairChat2.addMessage("frank", "testing");
+
+        Log.debug("Adding group chats");
         GroupChat groupChat1 = extChatDialog.createGroupChat("chat1@rooms.localhost", "luther.b",
                 GroupChatUser.PARTICIPANT);
         GroupChat groupChat2 = extChatDialog.createGroupChat("chat2@rooms.localhost", "luther",
                 GroupChatUser.PARTICIPANT);
 
+        Log.debug("Adding group users");
         GroupChatUser otherUser = new GroupChatUser("other user", "otheruser", "red", GroupChatUser.MODERADOR);
         GroupChatUser otherUser2 = new GroupChatUser("luther", "luther", "green", GroupChatUser.MODERADOR);
-
-        groupChat1.setSubject("Welcome to chat1, today topic: Cultural issues in Brazil");
         groupChat1.addUser(otherUser);
-        groupChat1.addMessage("luther.b", "Test message in group chat 1");
-
-        groupChat2.setSubject("Welcome to this room: we are talking today about 2009 meeting");
         groupChat2.addUser(otherUser2);
+
+        Log.debug("Adding subjects");
+        groupChat1.setSubject("Welcome to chat1, today topic: Cultural issues in Brazil");
+        groupChat2.setSubject("Welcome to this room: we are talking today about 2009 meeting");
+
+        Log.debug("Adding messages");
+        groupChat1.addMessage("luther.b", "Test message in group chat 1");
         groupChat2.addMessage("luther", "Mensaje de test en group chat 2");
+
+        Log.debug("Adding other messages");
         groupChat2.addInfoMessage("Mensaje de evento en group chat 2");
         groupChat2.addDelimiter("17:35");
 
