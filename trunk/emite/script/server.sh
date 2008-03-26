93c2032cf71d66c5bfca0497168ce0a77b0f445e
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-26 16:02:55 +0000

    Minor install and server.sh fixes

--- trunk/emite/script/server.sh
+++ trunk/emite/script/server.sh
@@ -4,5 +4,9 @@ if [ -d src/main/webapp/gwt/com.calclab.examplechat.Example ]
 then
   rm -rf src/main/webapp/gwt/com.calclab.examplechat.Example/
 fi
+if [ ! -d src/main/webapp/gwt ]
+then
+	mkdir src/main/webapp/gwt
+fi
 cp -a target/emite-0.0.1/com.calclab.examplechat.Example/ src/main/webapp/gwt/
 mvn jetty:run
