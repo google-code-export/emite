de6998689604531b681e241061b51e56a98a5786
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-03-26 11:50:57 +0000

    change jetty port to 8888

---
+++ trunk/emite/script/macserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -d src/main/webapp/gwt/com.calclab.examplechat.Example ]
+then
+  rm -rf src/main/webapp/gwt/com.calclab.examplechat.Example/
+fi
+cp -R target/emite-0.0.1/com.calclab.examplechat.Example/ src/main/webapp/gwt/
+mvn jetty:run
