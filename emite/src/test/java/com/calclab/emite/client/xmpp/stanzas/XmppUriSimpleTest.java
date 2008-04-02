4cd5e41024ebd24f351862e5129c9e75d340edbe
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-02 17:27:38 +0000

    XmppUri acepted without xmpp:

--- emite/src/test/java/com/calclab/emite/client/xmpp/stanzas/XmppUriSimpleTest.java
+++ emite/src/test/java/com/calclab/emite/client/xmpp/stanzas/XmppUriSimpleTest.java
@@ -6,57 +6,60 @@ import org.junit.Test;
 
 public class XmppUriSimpleTest {
 
-	@Test
-	public void checkUriFormat() {
-		final XmppURI uri = XmppURI.parseURI("xmpp:test@example/res");
-		assertEquals("test", uri.getJid().getNode());
-		assertEquals("example", uri.getJid().getHost());
-		assertEquals("res", uri.getResource());
-	}
+    @Test
+    public void checkUriFormat() {
+        final XmppURI uri = XmppURI.parseURI("xmpp:test@example/res");
+        assertEquals("test", uri.getJid().getNode());
+        assertEquals("example", uri.getJid().getHost());
+        assertEquals("res", uri.getResource());
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithOnlyAtFails() {
-		XmppURI.parseURI("xmpp:@");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithOnlyAtFails() {
+        XmppURI.parseURI("xmpp:@");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithOnlyHostFails() {
-		XmppURI.parseURI("xmpp:@example");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithOnlyHostFails() {
+        XmppURI.parseURI("xmpp:@example");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithOnlyNodeFails() {
-		XmppURI.parseURI("xmpp:test@");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithOnlyNodeFails() {
+        XmppURI.parseURI("xmpp:test@");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithOnlySlashFails() {
-		XmppURI.parseURI("xmpp:/");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithOnlySlashFails() {
+        XmppURI.parseURI("xmpp:/");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithoutHost() {
-		XmppURI.parseURI("xmpp:test@/res");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithoutHost() {
+        XmppURI.parseURI("xmpp:test@/res");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithoutHostFails() {
-		XmppURI.parseURI("xmpp:test/res");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithoutHostFails() {
+        XmppURI.parseURI("xmpp:test/res");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithoutNodeFails() {
-		XmppURI.parseURI("xmpp:@example/res");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithoutNodeFails() {
+        XmppURI.parseURI("xmpp:@example/res");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithoutResourceFails() {
-		XmppURI.parseURI("xmpp:test@example");
-	}
+    @Test(expected = RuntimeException.class)
+    public void checkUriFormatWithoutResourceFails() {
+        XmppURI.parseURI("xmpp:test@example");
+    }
 
-	@Test(expected = RuntimeException.class)
-	public void checkUriFormatWithoutXmpp() {
-		XmppURI.parseURI("test@example/res");
-	}
+    @Test
+    public void checkUriFormatWithoutXmpp() {
+        XmppURI uri = XmppURI.parseURI("test@example/res");
+        assertEquals("test", uri.getJid().getNode());
+        assertEquals("example", uri.getJid().getHost());
+        assertEquals("res", uri.getResource());
+    }
 
 }
