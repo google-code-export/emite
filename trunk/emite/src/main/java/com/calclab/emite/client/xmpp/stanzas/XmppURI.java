fb17b80fa6ae8f18d69055181ec98f67d77fd56c
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-02 17:27:38 +0000

    XmppUri acepted without xmpp:

--- trunk/emite/src/main/java/com/calclab/emite/client/xmpp/stanzas/XmppURI.java
+++ trunk/emite/src/main/java/com/calclab/emite/client/xmpp/stanzas/XmppURI.java
@@ -9,46 +9,47 @@ package com.calclab.emite.client.xmpp.stanzas;
  */
 
 public class XmppURI {
-	// TODO: no estoy seguro de las excepciones... mejor devolver null?
-	public static XmppURI parseURI(final String xmppUri) {
-		final String[] splitted1 = xmppUri.split("\\:");
-		if (splitted1.length != 2 || !splitted1[0].equals("xmpp")) {
-			throw new RuntimeException("Wrong XmppUri format" + xmppUri);
-		}
-		final String[] splitted2 = splitted1[1].split("/");
-		if (splitted2.length != 2 || !(splitted2[1].length() > 0)) {
-			throw new RuntimeException("Wrong XmppUri format" + xmppUri);
-		}
-		return new XmppURI(XmppJID.parseJID(splitted2[0]), splitted2[1]);
-	}
+    // TODO: no estoy seguro de las excepciones... mejor devolver null?
+    public static XmppURI parseURI(final String xmppUri) {
+        final String[] splitted1 = xmppUri.split("\\:");
+        if (splitted1.length > 2 || splitted1.length == 2 && !splitted1[0].equals("xmpp")) {
+            throw new RuntimeException("Wrong XmppUri format" + xmppUri);
+        }
+        String uriWithoutXmpp = splitted1.length == 1 ? splitted1[0] : splitted1[1];
+        final String[] splitted2 = uriWithoutXmpp.split("/");
+        if (splitted2.length != 2 || !(splitted2[1].length() > 0)) {
+            throw new RuntimeException("Wrong XmppUri format" + xmppUri);
+        }
+        return new XmppURI(XmppJID.parseJID(splitted2[0]), splitted2[1]);
+    }
 
-	private final XmppJID jid;
-	private final String resource;
+    private final XmppJID jid;
+    private final String resource;
 
-	/**
-	 * 
-	 * @param jid
-	 * @param resource
-	 *            <code> resource      = *( unreserved / escaped )
-	            reserved      = ";" / "/" / "?" / ":" / "@" / "&" / "=" / "+" /
-	            "$" / "," / "[" / "]" </code>
-	 * 
-	 */
-	public XmppURI(final XmppJID jid, final String resource) {
-		this.jid = jid;
-		this.resource = resource;
-	}
+    /**
+     * 
+     * @param jid
+     * @param resource
+     *                <code> resource      = *( unreserved / escaped )
+                reserved      = ";" / "/" / "?" / ":" / "@" / "&" / "=" / "+" /
+                "$" / "," / "[" / "]" </code>
+     * 
+     */
+    public XmppURI(final XmppJID jid, final String resource) {
+        this.jid = jid;
+        this.resource = resource;
+    }
 
-	public XmppJID getJid() {
-		return jid;
-	}
+    public XmppJID getJid() {
+        return jid;
+    }
 
-	public String getResource() {
-		return resource;
-	}
+    public String getResource() {
+        return resource;
+    }
 
-	@Override
-	public String toString() {
-		return "xmpp:" + jid + "/" + resource;
-	}
+    @Override
+    public String toString() {
+        return "xmpp:" + jid + "/" + resource;
+    }
 }
