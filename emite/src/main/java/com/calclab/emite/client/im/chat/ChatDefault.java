27632cffcd0b3d32e36a23cb206f4c00a07952e2
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-03 23:39:22 +0000

    UI better integrated with Chat class

--- emite/src/main/java/com/calclab/emite/client/im/chat/ChatDefault.java
+++ emite/src/main/java/com/calclab/emite/client/im/chat/ChatDefault.java
@@ -16,59 +16,68 @@ class ChatDefault implements Chat {
     private String to;
 
     public ChatDefault(final XmppURI other, final XmppURI myself, final String thread, final ChatManagerDefault manager) {
-	this.other = other;
-	this.thread = thread;
-	this.from = myself.toString();
-	this.to = other.toString();
-	this.manager = manager;
-	this.listeners = new ArrayList<ChatListener>();
-	this.hasThread = !(thread == null || thread.length() == 0);
-	this.id = createID(other, thread);
+        this.other = other;
+        this.thread = thread;
+        this.from = myself.toString();
+        this.to = other.toString();
+        this.manager = manager;
+        this.listeners = new ArrayList<ChatListener>();
+        this.hasThread = !(thread == null || thread.length() == 0);
+        this.id = createID(other, thread);
     }
 
     public void addListener(final ChatListener listener) {
-	listeners.add(listener);
+        listeners.add(listener);
     }
 
     public String getID() {
-	return id;
+        return id;
     }
 
     public XmppURI getOtherURI() {
-	return other;
+        return other;
     }
 
     public void send(final String body) {
-	final Message message = new Message(from, to, body);
-	manager.sendMessage(message);
-	for (final ChatListener listener : listeners) {
-	    listener.onMessageSent(this, message);
-	}
+        final Message message = new Message(from, to, body);
+        manager.sendMessage(message);
+        for (final ChatListener listener : listeners) {
+            listener.onMessageSent(this, message);
+        }
     }
 
     public void setOtherURI(final XmppURI otherURI) {
-	other = otherURI;
-	to = other.toString();
-	id = createID(other, thread);
+        other = otherURI;
+        to = other.toString();
+        id = createID(other, thread);
     }
 
     @Override
     public String toString() {
-	return id;
+        return id;
     }
 
     private String createID(final XmppURI other, final String thread) {
-	return "chat: " + other.toString() + (hasThread ? "-" + thread : "");
+        return "chat: " + other.toString() + (hasThread ? "-" + thread : "");
     }
 
     String getThread() {
-	return thread;
+        return thread;
     }
 
     void process(final Message message) {
-	for (final ChatListener listener : listeners) {
-	    listener.onMessageReceived(this, message);
-	}
+        for (final ChatListener listener : listeners) {
+            listener.onMessageReceived(this, message);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (other == null ? 0 : other.hashCode());
+        result = prime * result + (thread == null ? 0 : thread.hashCode());
+        return result;
     }
 
 }
