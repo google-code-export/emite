ec2ca337cd12fdbf7abeecd626bace9c544113a1
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-04 09:23:56 +0000

    better logs

--- trunk/emite/src/main/java/com/calclab/emite/client/core/services/gwt/GWTConnector.java
+++ trunk/emite/src/main/java/com/calclab/emite/client/core/services/gwt/GWTConnector.java
@@ -13,15 +13,17 @@ import com.google.gwt.http.client.Response;
 public class GWTConnector implements Connector {
     public void send(final String httpBase, final String request, final ConnectorCallback callback)
             throws ConnectorException {
-        Log.debug("SENDING: " + request);
+        Log.debug("GWT CONNECTOR SEND: " + request);
         final RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, httpBase);
         try {
             builder.sendRequest(request, new RequestCallback() {
                 public void onError(final Request arg0, final Throwable throwable) {
+                    Log.debug("GWT CONNECTOR ERROR: " + throwable);
                     callback.onError(throwable);
                 }
 
                 public void onResponseReceived(final Request req, final Response res) {
+                    Log.debug("GWT CONNECTOR RECEIVED: " + res.getText());
                     callback.onResponseReceived(res.getStatusCode(), res.getText());
                 }
             });
