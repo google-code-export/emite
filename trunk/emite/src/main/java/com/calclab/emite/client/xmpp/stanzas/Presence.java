b19dff2dfc46a28f1331c9c1cee1a33fb9f7e331
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-04 20:55:32 +0000

    Copyright added to files (II)

--- trunk/emite/src/main/java/com/calclab/emite/client/xmpp/stanzas/Presence.java
+++ trunk/emite/src/main/java/com/calclab/emite/client/xmpp/stanzas/Presence.java
@@ -25,99 +25,98 @@ import com.calclab.emite.client.core.packet.Packet;
 
 public class Presence extends BasicStanza {
     public static enum Show {
-	available, // Dani: he añadido available que faltaba, espero
-	// que no
-	// afecte...
-	away, chat, dnd, xa
+        // TODO: Dani: he añadido 'available' que faltaba, espero que no
+        // afecte...
+        available, away, chat, dnd, xa
     }
 
     public enum Type {
-	available, error, probe, subscribe, subscribed, unavailable, unsubscribe, unsubscribed
+        available, error, probe, subscribe, subscribed, unavailable, unsubscribe, unsubscribed
     }
 
     public Presence() {
-	this(null, null, null);
+        this(null, null, null);
     }
 
     public Presence(final Packet stanza) {
-	super(stanza);
+        super(stanza);
     }
 
     public Presence(final Type type, final XmppURI from, final XmppURI to) {
-	super("presence", "jabber:client");
-	if (type != null) {
-	    setType(type.toString());
-	}
-	if (from != null) {
-	    setFrom(from.toString());
-	}
-	if (to != null) {
-	    setTo(to.toString());
-	}
+        super("presence", "jabber:client");
+        if (type != null) {
+            setType(type.toString());
+        }
+        if (from != null) {
+            setFrom(from.toString());
+        }
+        if (to != null) {
+            setTo(to.toString());
+        }
     }
 
     public Presence(final XmppURI from) {
-	this(null, from, null);
+        this(null, from, null);
     }
 
     public int getPriority() {
-	int value = 0;
-	final Packet priority = getFirstChild("priority");
-	if (priority != null) {
-	    try {
-		value = Integer.parseInt(priority.getText());
-	    } catch (final NumberFormatException e) {
-		value = 0;
-	    }
-	}
-	return value;
+        int value = 0;
+        final Packet priority = getFirstChild("priority");
+        if (priority != null) {
+            try {
+                value = Integer.parseInt(priority.getText());
+            } catch (final NumberFormatException e) {
+                value = 0;
+            }
+        }
+        return value;
     }
 
     public Show getShow() {
-	final Packet show = getFirstChild("show");
-	final String value = show != null ? show.getText() : null;
-	return value != null ? Show.valueOf(value) : null;
+        final Packet show = getFirstChild("show");
+        final String value = show != null ? show.getText() : null;
+        return value != null ? Show.valueOf(value) : null;
     }
 
     public String getStatus() {
-	final Packet status = getFirstChild("status");
-	return status != null ? status.getText() : null;
+        final Packet status = getFirstChild("status");
+        return status != null ? status.getText() : null;
     }
 
     // TODO: revisar esto (type == null -> available)
     // http://www.xmpp.org/rfcs/rfc3921.html#presence
     public Type getType() {
-	final String type = getAttribute(BasicStanza.TYPE);
-	return type != null ? Type.valueOf(type) : Type.available;
+        final String type = getAttribute(BasicStanza.TYPE);
+        return type != null ? Type.valueOf(type) : Type.available;
     }
 
     public void setPriority(final int value) {
-	Packet priority = getFirstChild("priority");
-	if (priority == null) {
-	    priority = add("priority", null);
-	}
-	priority.setText(Integer.toString(value >= 0 ? value : 0));
+        Packet priority = getFirstChild("priority");
+        if (priority == null) {
+            priority = add("priority", null);
+        }
+        priority.setText(Integer.toString(value >= 0 ? value : 0));
     }
 
     public void setShow(final Show value) {
-	Packet show = getFirstChild("show");
-	if (show == null) {
-	    show = add("show", null);
-	}
-	show.setText(value.toString());
+        Packet show = getFirstChild("show");
+        if (show == null) {
+            show = add("show", null);
+        }
+        show.setText(value.toString());
     }
 
     public void setStatus(final String statusMessage) {
-	Packet status = getFirstChild("status");
-	if (status == null) {
-	    status = add("status", null);
-	}
-	status.setText(statusMessage);
+        Packet status = getFirstChild("status");
+        if (status == null) {
+            status = add("status", null);
+        }
+        status.setText(statusMessage);
     }
 
     public Presence With(final Show value) {
-	setShow(value);
-	return this;
+        setShow(value);
+        return this;
     }
 
 }
--- trunk/emite/src/main/java/com/calclab/examplechat/client/chatuiplugin/groupchat/GroupChat.java
+++ trunk/emite/src/main/java/com/calclab/examplechat/client/chatuiplugin/groupchat/GroupChat.java
@@ -1,22 +1,24 @@
-/*
- * Copyright (C) 2007 The kune development team (see CREDITS for details)
- * This file is part of kune.
+/**
  *
- * Kune is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * ((e)) emite: A pure gwt (Google Web Toolkit) xmpp (jabber) library
  *
- * Kune is distributed in the hope that it will be useful,
+ * (c) 2008 The emite development team (see CREDITS for details)
+ * This file is part of emite.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 package com.calclab.examplechat.client.chatuiplugin.groupchat;
 
 import com.calclab.examplechat.client.chatuiplugin.abstractchat.AbstractChat;
