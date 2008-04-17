793925685333c8593379286fdad221daadfe6a24
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-17 09:42:08 +0000

    emiteui package renamed

---
+++ trunk/emite/src/test/java/com/calclab/emiteui/client/MockitoXmpp.java
@@ -0,0 +1,96 @@
+package com.calclab.emiteui.client;
+
+import static org.mockito.Mockito.mock;
+
+import com.calclab.emite.client.Xmpp;
+import com.calclab.emite.client.components.Container;
+import com.calclab.emite.client.components.DefaultContainer;
+import com.calclab.emite.client.core.bosh.BoshOptions;
+import com.calclab.emite.client.core.dispatcher.Dispatcher;
+import com.calclab.emite.client.core.dispatcher.DispatcherMonitor;
+import com.calclab.emite.client.core.packet.IPacket;
+import com.calclab.emite.client.extra.muc.RoomManager;
+import com.calclab.emite.client.im.chat.ChatManager;
+import com.calclab.emite.client.im.presence.PresenceManager;
+import com.calclab.emite.client.im.roster.Roster;
+import com.calclab.emite.client.im.roster.RosterManager;
+import com.calclab.emite.client.xmpp.session.Session;
+import com.calclab.emite.client.xmpp.stanzas.XmppURI;
+import com.calclab.emite.client.xmpp.stanzas.Presence.Show;
+
+public class MockitoXmpp extends Xmpp {
+
+    private final ChatManager chat;
+    private final Container components;
+    private final Dispatcher dispatcher;
+    private final PresenceManager presenceManager;
+    private final Roster roster;
+    private final RosterManager rosterManager;
+    private final Session session;
+    private final RoomManager roomManager;
+
+    public MockitoXmpp() {
+        super(new DefaultContainer(), mock(BoshOptions.class), new DispatcherMonitor() {
+            public void publishing(final IPacket packet) {
+            }
+        });
+        chat = mock(ChatManager.class);
+        components = mock(Container.class);
+        dispatcher = mock(Dispatcher.class);
+        presenceManager = mock(PresenceManager.class);
+        roster = mock(Roster.class);
+        rosterManager = mock(RosterManager.class);
+        session = mock(Session.class);
+        roomManager = mock(RoomManager.class);
+    }
+
+    @Override
+    public ChatManager getChatManager() {
+        return chat;
+    }
+
+    @Override
+    public Container getComponents() {
+        return components;
+    }
+
+    @Override
+    public Dispatcher getDispatcher() {
+        return dispatcher;
+    }
+
+    @Override
+    public PresenceManager getPresenceManager() {
+        return presenceManager;
+    }
+
+    @Override
+    public RoomManager getRoomManager() {
+        return roomManager;
+    }
+
+    @Override
+    public Roster getRoster() {
+        return roster;
+    }
+
+    @Override
+    public RosterManager getRosterManager() {
+        return rosterManager;
+    }
+
+    @Override
+    public Session getSession() {
+        return session;
+    }
+
+    @Override
+    public void login(final XmppURI uri, final String password, final Show show, final String status) {
+        throw new RuntimeException("not implemented");
+    }
+
+    @Override
+    public void logout() {
+        throw new RuntimeException("not implemented");
+    }
+}
