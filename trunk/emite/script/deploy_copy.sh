6c9055a4fd89a57bfc7b79678c9fc774fe4ae2d8
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-17 12:19:34 +0000

    more scripts

---
+++ trunk/emite/script/deploy_copy.sh
@@ -0,0 +1,4 @@
+
+cp -a target/emite-0.2.0/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/
+cp -a target/emite-0.2.0/com.calclab.emite.examples.chat.Chat src/main/webapp/gwt/
+
--- trunk/emite/script/server.sh
+++ trunk/emite/script/server.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-if [ -d src/main/webapp/gwt/com.calclab.emiteui.EmiteUI ]
-then
-  rm -rf src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/*
-fi
-
-if [ ! -d src/main/webapp/gwt ]
-then
-	mkdir src/main/webapp/gwt
-fi
+./script/clean_deploy.sh
+./script/deploy_copy.sh
 
 
-cp -a target/emite-0.0.1/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/
 mvn jetty:run
