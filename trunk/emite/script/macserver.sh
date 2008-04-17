a6509efa67687506caad10cead714b803340128a
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-17 12:22:13 +0000

    0.2.0 in scripts renamed

--- trunk/emite/script/macserver.sh
+++ trunk/emite/script/macserver.sh
@@ -5,5 +5,5 @@ if [ ! -d src/main/webapp/gwt ]
 then
   mkdir src/main/webapp/gwt
 fi
-cp -R target/emite-0.0.1/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/
+cp -R target/emite-0.2.0/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/
 mvn jetty:run
