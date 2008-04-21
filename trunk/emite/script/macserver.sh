033170a449dab942d99963c676956c7537e523f2
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-21 00:11:23 +0000

    swing mejorado

--- trunk/emite/script/macserver.sh
+++ trunk/emite/script/macserver.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 rm -rf src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/
+
+
+
 if [ ! -d src/main/webapp/gwt ]
 then
   mkdir src/main/webapp/gwt
 fi
+
 cp -R target/emite-0.2.0/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/
+cp -R target/emite-0.2.0/com.calclab.emite.examples.chat.Chat src/main/webapp/gwt/com.calclab.emite.examples.chat.Chat/
+
 mvn jetty:run
