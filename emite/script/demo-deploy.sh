380e7df80a79f0f96e8e62e37f188e7a57a9cfc7
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-23 20:56:13 +0000

    'demo-deploy.sh danigb' must work

--- emite/script/demo-deploy.sh
+++ emite/script/demo-deploy.sh
@@ -3,6 +3,6 @@ PARAM=$#
 USER=$1
 if [ $PARAM -eq 1 ]
 then
-  EXTRA=$USER
+  EXTRA=$USER@
 fi
-rsync --no-g --progress -C -r target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/ ${EXTRA}@ourproject.org:/home/groups/kune/htdocs/emitedemo/
+rsync --no-g --progress -C -r target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/ ${EXTRA}ourproject.org:/home/groups/kune/htdocs/emitedemo/
