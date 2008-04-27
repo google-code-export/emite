fe93f3148e40fca16307908cb4dd8c470f2830a3
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-27 18:11:04 +0000

    demo issues

--- emite/script/demo-deploy.sh
+++ emite/script/demo-deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 PARAM=$#
 USER=$1
+DEST=$2
 if [ $PARAM -eq 1 ]
 then
   EXTRA=$USER@
 fi
 
+if [ $PARAM -eq 2 ]
+then
+  EXTRADEST=$DEST/
+fi
+
 chmod -R g+rw target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/
-rsync --progress -C -r -p target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/ ${EXTRA}ourproject.org:/home/groups/kune/htdocs/emitedemo/
-rsync --progress -C -r -p target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/EmiteUIDemo.html  ${EXTRA}ourproject.org:/home/groups/kune/htdocs/emitedemo/index.html
+rsync --progress -C -r -p target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/ ${EXTRA}ourproject.org:/home/groups/kune/htdocs/emitedemo/$EXTRADEST
+rsync --progress -C -r -p target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/EmiteUIDemo.html  ${EXTRA}ourproject.org:/home/groups/kune/htdocs/emitedemo/${EXTRADEST}index.html
