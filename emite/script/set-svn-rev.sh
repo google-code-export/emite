12fa16423c60f0baaa56fd13cb5b7b57f51b0d59
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-04-28 15:00:50 +0000

    escape of status, TRACES, and set-svn-rev revised...

--- emite/script/set-svn-rev.sh
+++ emite/script/set-svn-rev.sh
@@ -2,4 +2,5 @@
 REV=`svn info --xml| grep -m 1 revision | cut -d \" -f 2`
 COMMITSPENDING=`svn status | grep -c ""`
 perl -p -i -e "s/(<title>Emite UI application )(.*)(<\/title>)/\$1r$REV+c$COMMITSPENDING \(inestable\)\$3/gi" src/main/java/com/calclab/emiteui/public/EmiteUI.html
+perl -p -i -e "s/(<title>Emite UI application )(.*)(<\/title>)/\$1r$REV+c$COMMITSPENDING \(inestable\)\$3/gi" src/main/java/com/calclab/emiteui/public/EmiteUIDemo.html
 
