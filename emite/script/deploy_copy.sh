5a6bc82fe30a1c54e363abf27001e4554dd651e8
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-05-01 00:41:13 +0000

    small tweaks for 0.2 release

--- emite/script/deploy_copy.sh
+++ emite/script/deploy_copy.sh
@@ -1,4 +1,3 @@
 
-cp -a target/emite-0.2.0/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/
-cp -a target/emite-0.2.0/com.calclab.emite.examples.chat.Chat src/main/webapp/gwt/
+cp -R target/emite-0.2.0/com.calclab.emiteui.EmiteUI/std/* src/main/webapp/gwt/
 
