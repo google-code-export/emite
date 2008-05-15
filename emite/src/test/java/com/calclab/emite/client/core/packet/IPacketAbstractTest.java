9e93de02dfa8e764089442f4e3a7a1dde2ec1ff8
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-05-15 16:37:36 +0000

    packet test pass

--- emite/src/test/java/com/calclab/emite/client/core/packet/IPacketAbstractTest.java
+++ emite/src/test/java/com/calclab/emite/client/core/packet/IPacketAbstractTest.java
@@ -14,43 +14,45 @@ public abstract class IPacketAbstractTest {
 
     @Test
     public void getChildrenNeverReturnsNull() {
-        final IPacket packet = createPacket("root");
-        final List<? extends IPacket> children = packet.getChildren();
-        assertNotNull(children);
-        assertEquals(0, children.size());
+	final IPacket packet = createPacket("root");
+	final List<? extends IPacket> children = packet.getChildren();
+	assertNotNull(children);
+	assertEquals(0, children.size());
     }
 
     @Test
     public void getChildrenWithNameNotEmpty() {
-        IPacket packet = createPacket("<presence><x/><x/></presence");
-        assertEquals(2, packet.getChildren("x").size());
+	final IPacket packet = createPacket("presence");
+	packet.addChild(createPacket("x"));
+	packet.addChild(createPacket("x"));
+	assertEquals(2, packet.getChildren("x").size());
     }
 
     @Test
     public void getFirstChildNeverReturnsNull() {
-        final IPacket packet = createPacket("root");
-        final IPacket child = packet.getFirstChild("child");
-        assertNotNull(child);
-        assertSame(NoPacket.INSTANCE, child);
+	final IPacket packet = createPacket("root");
+	final IPacket child = packet.getFirstChild("child");
+	assertNotNull(child);
+	assertSame(NoPacket.INSTANCE, child);
     }
 
     @Test
     public void shouldSetAndClearTheAttributes() {
-        final IPacket packet = createPacket("packet");
-        packet.setAttribute("name", "value");
-        assertEquals("value", packet.getAttribute("name"));
-        packet.setAttribute("name", null);
-        assertNull(packet.getAttribute("name"));
-        assertFalse(packet.hasAttribute("name"));
+	final IPacket packet = createPacket("packet");
+	packet.setAttribute("name", "value");
+	assertEquals("value", packet.getAttribute("name"));
+	packet.setAttribute("name", null);
+	assertNull(packet.getAttribute("name"));
+	assertFalse(packet.hasAttribute("name"));
     }
 
     @Test
     public void shouldSetText() {
-        final IPacket packet = createPacket("packet");
-        packet.setText("text1");
-        assertEquals("text1", packet.getText());
-        packet.setText("text2");
-        assertEquals("text2", packet.getText());
+	final IPacket packet = createPacket("packet");
+	packet.setText("text1");
+	assertEquals("text1", packet.getText());
+	packet.setText("text2");
+	assertEquals("text2", packet.getText());
     }
 
     protected abstract IPacket createPacket(final String name);
