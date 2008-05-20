0189fc93f4c17af9db3c9c478d429e6378c10413
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-05-20 18:08:52 +0000

    ChatStateModule disabled by default

--- emite/src/main/java/com/calclab/emite/client/EmiteModule.java
+++ emite/src/main/java/com/calclab/emite/client/EmiteModule.java
@@ -24,7 +24,6 @@ package com.calclab.emite.client;
 import com.calclab.emite.client.core.CoreModule;
 import com.calclab.emite.client.core.services.gwt.GWTServicesModule;
 import com.calclab.emite.client.extra.avatar.AvatarModule;
-import com.calclab.emite.client.extra.chatstate.ChatStateModule;
 import com.calclab.emite.client.extra.muc.MUCModule;
 import com.calclab.emite.client.im.InstantMessagingModule;
 import com.calclab.emite.client.modular.Container;
@@ -35,23 +34,23 @@ import com.calclab.emite.client.xmpp.XMPPModule;
 public class EmiteModule implements Module {
 
     public static Xmpp getXmpp(final Container container) {
-        return container.getInstance(Xmpp.class);
+	return container.getInstance(Xmpp.class);
     }
 
     public static void load(final ModuleContainer container) {
-        container.add(new GWTServicesModule());
-        container.add(new CoreModule(), new XMPPModule(), new InstantMessagingModule());
-        // FIXME: esto debería ir fuera de aquí
-        container.add(new MUCModule(), new AvatarModule(), new ChatStateModule());
-        container.add(new EmiteModule());
+	container.add(new GWTServicesModule());
+	container.add(new CoreModule(), new XMPPModule(), new InstantMessagingModule());
+	// FIXME: esto debería ir fuera de aquí
+	container.add(new MUCModule(), new AvatarModule());
+	container.add(new EmiteModule());
     }
 
     public Class<? extends Module> getType() {
-        return EmiteModule.class;
+	return EmiteModule.class;
     }
 
     public void onLoad(final Container container) {
-        container.registerSingletonInstance(Xmpp.class, new Xmpp(container));
+	container.registerSingletonInstance(Xmpp.class, new Xmpp(container));
     }
 
 }
