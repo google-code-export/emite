1a4e3eda8e330ca73d71095129c83db6189d7205
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-06-06 04:41:08 +0000

    issue #54

--- trunk/emite/src/main/java/com/calclab/emiteuimodule/client/status/StatusUIPanel.java
+++ trunk/emite/src/main/java/com/calclab/emiteuimodule/client/status/StatusUIPanel.java
@@ -67,251 +67,264 @@ public class StatusUIPanel extends Toolbar implements StatusUIView {
     private final StatusUIPresenter presenter;
     private final Menu chatMenu;
     private Menu optionsSubMenu;
+    private final String ownStatusFootText;
+    private final String ownStatusHeadText;
 
     public StatusUIPanel(final StatusUIPresenter presenter, final I18nTranslationService i18n) {
-	this.presenter = presenter;
-	this.i18n = i18n;
+        this.presenter = presenter;
+        this.i18n = i18n;
 
-	chatMenu = new Menu();
-	chatMenu.setShadow(true);
+        chatMenu = new Menu();
+        chatMenu.setShadow(true);
 
-	closeAllOption = createCloseAllMenuItem(i18n);
+        closeAllOption = createCloseAllMenuItem(i18n);
 
-	final MenuItem optionsItem = new MenuItem(i18n.t("Options"), createOptionsMenu());
-	chatMenu.addItem(closeAllOption);
-	chatMenu.addItem(optionsItem);
+        final MenuItem optionsItem = new MenuItem(i18n.t("Options"), createOptionsMenu());
+        chatMenu.addItem(closeAllOption);
+        chatMenu.addItem(optionsItem);
 
-	final ToolbarButton chatMenuButton = new ToolbarButton(i18n.t("Chat"));
-	chatMenuButton.setMenu(chatMenu);
-	this.addButton(chatMenuButton);
-	this.addSeparator();
+        final ToolbarButton chatMenuButton = new ToolbarButton(i18n.t("Chat"));
+        chatMenuButton.setMenu(chatMenu);
+        this.addButton(chatMenuButton);
+        this.addSeparator();
 
-	statusMenu = createStatusMenu();
-	statusButton = new ToolbarButton("Set status");
-	statusButton.setMenu(statusMenu);
-	statusButton.setTooltip(i18n.t("Set status"));
-	this.addButton(statusButton);
+        statusMenu = createStatusMenu();
+        statusButton = new ToolbarButton("Set status");
+        statusButton.setMenu(statusMenu);
+        statusButton.setTooltip(i18n.t("Set status"));
+        this.addButton(statusButton);
 
-	loading = new ToolbarButton();
-	loading.setIcon("images/ext-load.gif");
-	loading.setCls("x-btn-icon");
-	this.addButton(loading);
-	setLoadingVisible(false);
+        loading = new ToolbarButton();
+        loading.setIcon("images/ext-load.gif");
+        loading.setCls("x-btn-icon");
+        this.addButton(loading);
+        setLoadingVisible(false);
 
-	this.addFill();
+        this.addFill();
+        ownStatusHeadText = "<b>";
+        ownStatusFootText = "</b><br/>" + i18n.t("(Click to change your status)");
     }
 
     public void addChatMenuItem(final View item) {
-	chatMenu.addItem((BaseItem) item);
+        chatMenu.addItem((BaseItem) item);
     }
 
     public void addMenuButtonItem(final View item) {
-	super.addButton((ToolbarButton) item);
+        super.addButton((ToolbarButton) item);
     }
 
     public void addMenuItem(final View item) {
-	super.addItem((ToolbarItem) item);
+        super.addItem((ToolbarItem) item);
     }
 
     public void addOptionsSubMenuItem(final View item) {
-	optionsSubMenu.addItem((BaseItem) item);
+        optionsSubMenu.addItem((BaseItem) item);
     }
 
     public void confirmCloseAll() {
-	MessageBox.confirm(i18n.t("Confirm"), i18n.t("Are you sure you want to exit all the chats?"),
-		new MessageBox.ConfirmCallback() {
-		    public void execute(final String btnID) {
-			if (btnID.equals("yes")) {
-			    DeferredCommand.addCommand(new Command() {
-				public void execute() {
-				    presenter.onCloseAllConfirmed();
-				}
-			    });
-			}
-		    }
-		});
+        MessageBox.confirm(i18n.t("Confirm"), i18n.t("Are you sure you want to exit all the chats?"),
+                new MessageBox.ConfirmCallback() {
+                    public void execute(final String btnID) {
+                        if (btnID.equals("yes")) {
+                            DeferredCommand.addCommand(new Command() {
+                                public void execute() {
+                                    presenter.onCloseAllConfirmed();
+                                }
+                            });
+                        }
+                    }
+                });
     }
 
     public void setCloseAllOptionEnabled(boolean enabled) {
-	closeAllOption.setDisabled(!enabled);
+        closeAllOption.setDisabled(!enabled);
     }
 
     public void setLoadingVisible(final boolean visible) {
-	loading.setVisible(visible);
+        loading.setVisible(visible);
     }
 
     public void setOwnPresence(final OwnPresence ownPresence) {
-	switch (ownPresence.getStatus()) {
-	case online:
-	    onlineMenuItem.setChecked(true);
-	    break;
-	case onlinecustom:
-	    onlineCustomMenuItem.setChecked(true);
-	    break;
-	case offline:
-	    offlineMenuItem.setChecked(true);
-	    break;
-	case busy:
-	    busyMenuItem.setChecked(true);
-	    break;
-	case busycustom:
-	    busyCustomMenuItem.setChecked(true);
-	    break;
-	}
+        switch (ownPresence.getStatus()) {
+        case online:
+            onlineMenuItem.setChecked(true);
+            setOwnStatusText(ownStatusHeadText + i18n.t("Online") + ownStatusFootText);
+            break;
+        case onlinecustom:
+            setOwnStatusText(ownStatusHeadText + ownPresence.getStatusText() + ownStatusFootText);
+            onlineCustomMenuItem.setChecked(true);
+            break;
+        case offline:
+            setOwnStatusText(ownStatusHeadText + i18n.t("Offline") + ownStatusFootText);
+            offlineMenuItem.setChecked(true);
+            break;
+        case busy:
+            setOwnStatusText(ownStatusHeadText + i18n.t("Don't disturb") + ownStatusFootText);
+            busyMenuItem.setChecked(true);
+            break;
+        case busycustom:
+            setOwnStatusText(ownStatusHeadText + ownPresence.getStatusText() + ownStatusFootText);
+            busyCustomMenuItem.setChecked(true);
+            break;
+        }
 
-	final String icon = ChatUIUtils.getOwnStatusIcon(ownPresence.getStatus()).getHTML();
-	statusButton.setText(icon);
+        final String icon = ChatUIUtils.getOwnStatusIcon(ownPresence.getStatus()).getHTML();
+        statusButton.setText(icon);
     }
 
     public void setSubscriptionMode(final SubscriptionMode mode) {
-	switch (mode) {
-	case autoAcceptAll:
-	    autoAcceptSubsItem.setChecked(true);
-	    break;
-	case autoRejectAll:
-	    autoRejectSubsItem.setChecked(true);
-	    break;
-	default:
-	    manualSubsItem.setChecked(true);
-	    break;
-	}
+        switch (mode) {
+        case autoAcceptAll:
+            autoAcceptSubsItem.setChecked(true);
+            break;
+        case autoRejectAll:
+            autoRejectSubsItem.setChecked(true);
+            break;
+        default:
+            manualSubsItem.setChecked(true);
+            break;
+        }
     }
 
     public void showAlert(final String message) {
-	MessageDialog.alert(i18n.t("Alert"), message);
+        MessageDialog.alert(i18n.t("Alert"), message);
     }
 
     private Item createCloseAllMenuItem(final I18nTranslationService i18n) {
-	final Item closeAllOption = new Item();
-	closeAllOption.setText(i18n.t("Close all chats"));
-	closeAllOption.setIconCls("exit-icon");
-	closeAllOption.addListener(new BaseItemListenerAdapter() {
-	    @Override
-	    public void onClick(final BaseItem item, final EventObject e) {
-		confirmCloseAll();
-	    }
-	});
-	return closeAllOption;
+        final Item closeAllOption = new Item();
+        closeAllOption.setText(i18n.t("Close all chats"));
+        closeAllOption.setIconCls("exit-icon");
+        closeAllOption.addListener(new BaseItemListenerAdapter() {
+            @Override
+            public void onClick(final BaseItem item, final EventObject e) {
+                confirmCloseAll();
+            }
+        });
+        return closeAllOption;
     }
 
     private Menu createOptionsMenu() {
-	optionsSubMenu = new Menu();
-	final ColorMenu colorMenu = new ColorMenu();
-	colorMenu.addListener(new ColorMenuListener() {
-	    public void onSelect(final ColorPalette colorPalette, final String color) {
-		DeferredCommand.addCommand(new Command() {
-		    public void execute() {
-			presenter.onUserColorChanged(color);
-		    }
-		});
-	    }
-	});
+        optionsSubMenu = new Menu();
+        final ColorMenu colorMenu = new ColorMenu();
+        colorMenu.addListener(new ColorMenuListener() {
+            public void onSelect(final ColorPalette colorPalette, final String color) {
+                DeferredCommand.addCommand(new Command() {
+                    public void execute() {
+                        presenter.onUserColorChanged(color);
+                    }
+                });
+            }
+        });
 
-	final MenuItem colorMenuItem = new MenuItem(i18n.t("Choose your color"), colorMenu);
-	colorMenuItem.setIconCls("colors-icon");
-	colorMenuItem.setIcon("images/colors.gif");
+        final MenuItem colorMenuItem = new MenuItem(i18n.t("Choose your color"), colorMenu);
+        colorMenuItem.setIconCls("colors-icon");
+        colorMenuItem.setIcon("images/colors.gif");
 
-	final MenuItem subsItem = new MenuItem(i18n.t("New buddies options"), createUserSubscriptionMenu());
+        final MenuItem subsItem = new MenuItem(i18n.t("New buddies options"), createUserSubscriptionMenu());
 
-	optionsSubMenu.addItem(colorMenuItem);
-	optionsSubMenu.addItem(subsItem);
+        optionsSubMenu.addItem(colorMenuItem);
+        optionsSubMenu.addItem(subsItem);
 
-	return optionsSubMenu;
+        return optionsSubMenu;
     }
 
     private CheckItem createStatusCheckItem(final OwnStatus ownStatus) {
-	final CheckItem checkItem = new CheckItem();
-	checkItem.setText(ChatUIUtils.getOwnStatusIconAndText(i18n, ownStatus));
-	checkItem.setGroup("chatstatus");
-	switch (ownStatus) {
-	case offline:
-	case online:
-	case busy:
-	    checkItem.addListener(new BaseItemListenerAdapter() {
-		@Override
-		public void onClick(final BaseItem item, final EventObject e) {
-		    DeferredCommand.addCommand(new Command() {
-			public void execute() {
-			    presenter.setOwnPresence(new OwnPresence(ownStatus));
-			}
-		    });
-		}
-	    });
-	    break;
-	case busycustom:
-	case onlinecustom:
-	    checkItem.addListener(new BaseItemListenerAdapter() {
-		@Override
-		public void onClick(final BaseItem item, final EventObject e) {
-		    MessageBox.prompt(i18n.t("Set your status message"), i18n
-			    .t("Set your status text (something like 'Out for dinner' or 'Working')"),
-			    new PromptCallback() {
-				public void execute(final String btnID, final String text) {
-				    DeferredCommand.addCommand(new Command() {
-					public void execute() {
-					    presenter.setOwnPresence(new OwnPresence(ownStatus, text));
-					}
-				    });
-				}
-			    });
-		}
-	    });
-	    break;
-	}
-	return checkItem;
+        final CheckItem checkItem = new CheckItem();
+        checkItem.setText(ChatUIUtils.getOwnStatusIconAndText(i18n, ownStatus));
+        checkItem.setGroup("chatstatus");
+        switch (ownStatus) {
+        case offline:
+        case online:
+        case busy:
+            checkItem.addListener(new BaseItemListenerAdapter() {
+                @Override
+                public void onClick(final BaseItem item, final EventObject e) {
+                    DeferredCommand.addCommand(new Command() {
+                        public void execute() {
+                            presenter.setOwnPresence(new OwnPresence(ownStatus));
+                        }
+                    });
+                }
+            });
+            break;
+        case busycustom:
+        case onlinecustom:
+            checkItem.addListener(new BaseItemListenerAdapter() {
+                @Override
+                public void onClick(final BaseItem item, final EventObject e) {
+                    MessageBox.prompt(i18n.t("Set your status message"), i18n
+                            .t("Set your status text (something like 'Out for dinner' or 'Working')"),
+                            new PromptCallback() {
+                                public void execute(final String btnID, final String text) {
+                                    DeferredCommand.addCommand(new Command() {
+                                        public void execute() {
+                                            presenter.setOwnPresence(new OwnPresence(ownStatus, text));
+                                        }
+                                    });
+                                }
+                            });
+                }
+            });
+            break;
+        }
+        return checkItem;
     }
 
     private Menu createStatusMenu() {
-	final Menu statusMenu = new Menu();
-	statusMenu.setShadow(true);
-	statusMenu.addItem(new TextItem("<b style=\"margin: 5px 0px;\" class=\"menu-title\">"
-		+ i18n.t("Change your status") + "</b>"));
-	onlineMenuItem = createStatusCheckItem(OwnStatus.online);
-	onlineCustomMenuItem = createStatusCheckItem(OwnStatus.onlinecustom);
-	busyMenuItem = createStatusCheckItem(OwnStatus.busy);
-	busyCustomMenuItem = createStatusCheckItem(OwnStatus.busycustom);
-	offlineMenuItem = createStatusCheckItem(OwnStatus.offline);
-	statusMenu.addItem(onlineMenuItem);
-	statusMenu.addItem(onlineCustomMenuItem);
-	statusMenu.addItem(busyMenuItem);
-	statusMenu.addItem(busyCustomMenuItem);
-	statusMenu.addSeparator();
-	statusMenu.addItem(offlineMenuItem);
-	return statusMenu;
+        final Menu statusMenu = new Menu();
+        statusMenu.setShadow(true);
+        statusMenu.addItem(new TextItem("<b style=\"margin: 5px 0px;\" class=\"menu-title\">"
+                + i18n.t("Change your status") + "</b>"));
+        onlineMenuItem = createStatusCheckItem(OwnStatus.online);
+        onlineCustomMenuItem = createStatusCheckItem(OwnStatus.onlinecustom);
+        busyMenuItem = createStatusCheckItem(OwnStatus.busy);
+        busyCustomMenuItem = createStatusCheckItem(OwnStatus.busycustom);
+        offlineMenuItem = createStatusCheckItem(OwnStatus.offline);
+        statusMenu.addItem(onlineMenuItem);
+        statusMenu.addItem(onlineCustomMenuItem);
+        statusMenu.addItem(busyMenuItem);
+        statusMenu.addItem(busyCustomMenuItem);
+        statusMenu.addSeparator();
+        statusMenu.addItem(offlineMenuItem);
+        return statusMenu;
     }
 
     private CheckItem createSubscritionItem(final String text, final Menu submenu, final SubscriptionMode mode) {
-	final CheckItemListenerAdapter itemListener = new CheckItemListenerAdapter() {
-	    @Override
-	    public void onCheckChange(CheckItem item, boolean checked) {
-		if (checked) {
-		    DeferredCommand.addCommand(new Command() {
-			public void execute() {
-			    presenter.onUserSubscriptionModeChanged(mode);
-			}
-		    });
-		}
-	    }
-	};
-	final CheckItem item = new CheckItem();
-	item.setText(text);
-	item.setGroup("subscription");
-	item.addListener(itemListener);
-	submenu.addItem(item);
-	return item;
+        final CheckItemListenerAdapter itemListener = new CheckItemListenerAdapter() {
+            @Override
+            public void onCheckChange(CheckItem item, boolean checked) {
+                if (checked) {
+                    DeferredCommand.addCommand(new Command() {
+                        public void execute() {
+                            presenter.onUserSubscriptionModeChanged(mode);
+                        }
+                    });
+                }
+            }
+        };
+        final CheckItem item = new CheckItem();
+        item.setText(text);
+        item.setGroup("subscription");
+        item.addListener(itemListener);
+        submenu.addItem(item);
+        return item;
     }
 
     private Menu createUserSubscriptionMenu() {
-	final Menu submenu = new Menu();
-	submenu.setShadow(true);
-	submenu.setMinWidth(10);
-	autoAcceptSubsItem = createSubscritionItem(i18n
-		.t("Automatically accept users as buddies when a user request it"), submenu,
-		SubscriptionMode.autoAcceptAll);
-	autoRejectSubsItem = createSubscritionItem(i18n.t("Automatically reject new buddies inclusion requests"),
-		submenu, SubscriptionMode.autoRejectAll);
-	manualSubsItem = createSubscritionItem(i18n.t("Manual accept or reject new buddies inclusion requests"),
-		submenu, SubscriptionMode.manual);
-	return submenu;
+        final Menu submenu = new Menu();
+        submenu.setShadow(true);
+        submenu.setMinWidth(10);
+        autoAcceptSubsItem = createSubscritionItem(i18n
+                .t("Automatically accept users as buddies when a user request it"), submenu,
+                SubscriptionMode.autoAcceptAll);
+        autoRejectSubsItem = createSubscritionItem(i18n.t("Automatically reject new buddies inclusion requests"),
+                submenu, SubscriptionMode.autoRejectAll);
+        manualSubsItem = createSubscritionItem(i18n.t("Manual accept or reject new buddies inclusion requests"),
+                submenu, SubscriptionMode.manual);
+        return submenu;
+    }
+
+    private void setOwnStatusText(final String text) {
+        statusButton.setTooltip(text);
     }
 }
