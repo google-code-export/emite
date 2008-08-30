c44f952f1f0066bff5fc40cb6eba5bd4456ccff2
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-08-30 14:07:55 +0000

    0.3.3, pom.xml not generates suco jar, other minor changes

--- emite/script/upload_widgets.dani.sh
+++ emite/script/upload_widgets.dani.sh
@@ -1,2 +1,2 @@
-rsync --progress -C -r -p target/emite-0.3.2/com.calclab.emite.widgets.demo.EmiteWidgetsDemo/ danigb@ourproject.org:/home/groups/kune/htdocs/emitedemo/widgetsdev
+rsync --progress -C -r -p target/emite-0.3.3/com.calclab.emite.widgets.demo.EmiteWidgetsDemo/ danigb@ourproject.org:/home/groups/kune/htdocs/emitedemo/widgetsdev
 
