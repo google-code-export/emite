b9a33c49ca77460872d0cfd21f778e0843c5a15e
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2008-09-15 20:13:28 +0000

    Emite-JS under development!

--- trunk/emite-js/src/main/java/com/calclab/emite/js/client/EmiteJSEntryPoint.java
+++ trunk/emite-js/src/main/java/com/calclab/emite/js/client/EmiteJSEntryPoint.java
@@ -1,11 +1,11 @@
 package com.calclab.emite.js.client;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.EntryPoint;
 
 public class EmiteJSEntryPoint implements EntryPoint {
-
     public void onModuleLoad() {
-
+	Log.debug("Emite-JS ready.");
     }
 
 }
