9cee4d22b12b2b52bc066d981cbe552a7c1c14bb
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-10-03 09:54:35 +0000

    minor changes

--- trunk/emite/script/signalGen.sh
+++ trunk/emite/script/signalGen.sh
@@ -8,7 +8,7 @@ if [ $PARAMS -lt 1 ]
 then
   echo "Use: $0 signalName1.ParamType signalName2.ParamType ... signalNameN.ParamType"
   echo "$0 onEvent1.String onEvent2.Long onEvent3.String"
-  echo "$0 onEvent1. onEvent2. onEvent3. # for Signal0/Slot0"
+  echo "$0 onEvent1. onEvent2. onEvent3. # for Event0/Listener0"
   exit
 fi
 
@@ -16,21 +16,21 @@ echo ""
 
 for NAME in $NAMES
 do
-  SIGNALNAME=${NAME/%\.*/}
-  SIGNALPARAM=${NAME/#*./}
+  EVENTNAME=${NAME/%\.*/}
+  EVENTPARAM=${NAME/#*./}
 
-  if [[ -n $SIGNALPARAM ]]
+  if [[ -n $EVENTPARAM ]]
   then
-    SIGNALPARAM="<$SIGNALPARAM>"
+    EVENTPARAM="<$EVENTPARAM>"
   else
-    SIGNALPARAM="0"
+    EVENTPARAM="0"
   fi
 
-  echo "private final Signal$SIGNALPARAM $SIGNALNAME;"
-  echo "public void $SIGNALNAME(final Slot$SIGNALPARAM slot) {
-  	$SIGNALNAME.add(slot);
+  echo "private final Event$EVENTPARAM $EVENTNAME;"
+  echo "public void $EVENTNAME(final Listener$EVENTPARAM slot) {
+  	$EVENTNAME.add(slot);
       }"
   echo ""
-  echo "this.$SIGNALNAME = new Signal$SIGNALPARAM(\"$SIGNALNAME\");"
+  echo "this.$EVENTNAME = new Event$EVENTPARAM(\"$EVENTNAME\");"
   echo ""
 done
