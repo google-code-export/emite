5b49bb87e7c8c02a058786b9d910e5b0ca9d9202
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2008-10-09 19:05:54 +0000

    removing some thread warnings

--- trunk/emite/src/test/java/com/calclab/emiteuimodule/client/selenium/EmiteUISeleniumLoginLogoutTest.java
+++ trunk/emite/src/test/java/com/calclab/emiteuimodule/client/selenium/EmiteUISeleniumLoginLogoutTest.java
@@ -4,55 +4,58 @@ import junit.framework.TestCase;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 
-public abstract class EmiteUISeleniumLoginLogoutTest extends TestCase {
+public class EmiteUISeleniumLoginLogoutTest extends TestCase {
 
     public void testOnlineAndOffline() throws Exception {
-	final DefaultSelenium selenium = createSeleniumClient("http://localhost:4444/");
-	selenium.start();
+        final DefaultSelenium selenium = createSeleniumClient("http://localhost:4444/");
+        selenium.start();
 
-	try {
-	    selenium.open("/gwt/com.calclab.emiteui.EmiteUI/EmiteUI.html");
-	    selenium.click("EmiteDemoLoginPanel-online-button");
-	    for (int second = 0;; second++) {
-		if (second >= 60) {
-		    fail("timeout");
-		}
-		try {
-		    if (selenium.getText("gwt-debug-MultiChatPanel-InfoLabel").matches(
-			    "^To start a chat, select a buddy or join to a chat room[\\s\\S]*$")) {
-			break;
-		    }
-		} catch (final Exception e) {
-		}
-		Thread.sleep(1000);
-	    }
+        try {
+            selenium.open("/gwt/com.calclab.emiteui.EmiteUI/EmiteUI.html");
+            selenium.click("EmiteDemoLoginPanel-online-button");
+            for (int second = 0;; second++) {
+                if (second >= 60) {
+                    fail("timeout");
+                }
+                try {
+                    if (selenium.getText("gwt-debug-MultiChatPanel-InfoLabel").matches(
+                            "^To start a chat, select a buddy or join to a chat room[\\s\\S]*$")) {
+                        break;
+                    }
+                } catch (final Exception e) {
+                }
+                Thread.sleep(1000);
+            }
 
-	    selenium.click("EmiteDemoLoginPanel-offline-button");
-	    for (int second = 0;; second++) {
-		if (second >= 60) {
-		    fail("timeout");
-		}
-		try {
-		    if ("To start a chat you need to be 'online'.".equals(selenium
-			    .getText("gwt-debug-MultiChatPanel-InfoLabel"))) {
-			break;
-		    }
-		} catch (final Exception e) {
-		}
-		Thread.sleep(1000);
-	    }
-	    selenium.stop();
-	} catch (final UnsupportedOperationException e) {
-	    System.err.println("Seems that selenium server is not running; run before: 'mvn selenium:start-server' ");
-	}
+            selenium.click("EmiteDemoLoginPanel-offline-button");
+            for (int second = 0;; second++) {
+                if (second >= 60) {
+                    fail("timeout");
+                }
+                try {
+                    if ("To start a chat you need to be 'online'.".equals(selenium
+                            .getText("gwt-debug-MultiChatPanel-InfoLabel"))) {
+                        break;
+                    }
+                } catch (final Exception e) {
+                }
+                Thread.sleep(1000);
+            }
+            selenium.stop();
+        } catch (final UnsupportedOperationException e) {
+            System.err.println("Seems that selenium server is not running; run before: 'mvn selenium:start-server' ");
+        }
 
     }
 
     protected DefaultSelenium createSeleniumClient(final String url) throws Exception {
-	// return new DefaultSelenium("localhost", 4441,
-	// "*firefox /usr/lib/firefox-3.0.3/firefox", url);
+        // ff3 hangs: http://jira.openqa.org/browse/SRC-225
+        // as a workarount use ff2:
+        //
+        // return new DefaultSelenium("localhost", 4441,
+        // "*firefox /usr/lib/firefox-3.0.3/firefox", url);
 
-	// this is a problem... platform dependence ...
-	return new DefaultSelenium("localhost", 4441, "*firefox /usr/lib/firefox/firefox-2-bin", url);
+        // this is a problem... platform dependence ...
+        return new DefaultSelenium("localhost", 4441, "*firefox /usr/lib/firefox/firefox-2-bin", url);
     }
 }
