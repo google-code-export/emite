51d1323fd5b3bb9af3a02e93a434def04ba9deff
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2009-01-30 16:32:07 +0000

    lgpl and (c) 2008-2009

--- emite/script/addCopyrightToFiles.sh
+++ emite/script/addCopyrightToFiles.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `./script/findFilesWithoutCopyright.sh` ;do cp COPYRIGHT-AGPLv3.java /tmp/copy; cat $i >> /tmp/copy ; mv /tmp/copy $i ; done
+for i in `./script/findFilesWithoutCopyright.sh` ;do cp COPYRIGHT-LGPLv3.java /tmp/copy; cat $i >> /tmp/copy ; mv /tmp/copy $i ; done
