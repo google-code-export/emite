3417f7090875c095810a2cfcc6ff6c86bb3aabac
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2009-02-13 12:13:22 +0000

    0.4.6 after 0.4.5 jar corrupted

--- trunk/emite/script/macserver.sh
+++ trunk/emite/script/macserver.sh
@@ -9,7 +9,7 @@ then
   mkdir src/main/webapp/gwt
 fi
 
-cp -R target/emite-0.4.5/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/
-cp -R target/emite-0.4.5/com.calclab.emite.examples.chat.Chat/ src/main/webapp/gwt/com.calclab.emite.examples.chat.Chat/
+cp -R target/emite-0.4.6/com.calclab.emiteui.EmiteUI/ src/main/webapp/gwt/com.calclab.emiteui.EmiteUI/
+cp -R target/emite-0.4.6/com.calclab.emite.examples.chat.Chat/ src/main/webapp/gwt/com.calclab.emite.examples.chat.Chat/
 
 mvn jetty:run
