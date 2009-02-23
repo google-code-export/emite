73f233951d8cdaeefdf5010976eae44807b43334
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2009-02-23 13:03:56 +0000

    First status demo working!

--- trunk/emite-js/src/main/java/com/calclab/emite/js/client/Callback.java
+++ trunk/emite-js/src/main/java/com/calclab/emite/js/client/Callback.java
@@ -3,10 +3,12 @@ package com.calclab.emite.js.client;
 import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.ExportClosure;
 import org.timepedia.exporter.client.ExportPackage;
+import org.timepedia.exporter.client.Exportable;
 
 @Export
 @ExportPackage("emitexmpp")
 @ExportClosure
-public interface Callback {
+public interface Callback extends Exportable {
+    @Export
     public void onEvent(String param);
 }
