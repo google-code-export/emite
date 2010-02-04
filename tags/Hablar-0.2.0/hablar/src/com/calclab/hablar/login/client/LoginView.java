34584c56c5afb8ea283f9bb28944092ef3095e8b
danigb <danigb@87f8d446-a148-0410-9e96-a52f46714620>
2010-02-04 10:21:59 +0000

    Second (small) iteration for Surevine

---
+++ tags/Hablar-0.2.0/hablar/src/com/calclab/hablar/login/client/LoginView.java
@@ -0,0 +1,7 @@
+package com.calclab.hablar.login.client;
+
+import com.calclab.hablar.basic.client.ui.page.PageView;
+
+public interface LoginView extends PageView {
+
+}
---
+++ tags/Hablar-0.2.0/hablar/test/com/calclab/hablar/selenium/tools/GenericWebTester.java
@@ -0,0 +1,11 @@
+package com.calclab.hablar.selenium.tools;
+
+import org.openqa.selenium.WebDriver;
+
+public class GenericWebTester extends AbstractWebTester {
+
+    public GenericWebTester(final WebDriver driver, final String baseUrl) {
+	super(driver, baseUrl);
+    }
+
+}
