d96b58016a95a16b1151ac01252da22f2b37539b
vruiz.jurado <vruiz.jurado@87f8d446-a148-0410-9e96-a52f46714620>
2010-02-12 05:04:50 +0000

    VCard tests updated

--- emite-functional/src/com/calclab/emite/functionaltester/client/tests/VCardTestSuite.java
+++ emite-functional/src/com/calclab/emite/functionaltester/client/tests/VCardTestSuite.java
@@ -11,25 +11,28 @@ public class VCardTestSuite extends BasicTestSuite {
 
     private final FunctionalTest requestVCard = new FunctionalTest() {
 
-	@Override
-	public void run(final Context ctx) {
-	    ctx.info("Starting request vcard test");
-	    manager = Suco.get(VCardManager.class);
-	    manager.requestOwnVCard(new Listener<VCardResponse>() {
-		@Override
-		public void onEvent(VCardResponse response) {
-		    ctx.success("Discovery features received");
-		    ctx.info("Response: " + response.getType().toString());
-		    ctx.getSession().logout();
-		}
-	    });
-	}
+        @Override
+        public void run(final Context ctx) {
+            ctx.info("Starting request vcard test");
+            manager = Suco.get(VCardManager.class);
+            manager.requestOwnVCard(new Listener<VCardResponse>() {
+                @Override
+                public void onEvent(final VCardResponse response) {
+                    ctx.success("Discovery features received");
+                    ctx.info("Response: " + response.getType().toString());
+                    ctx.info("Response hasVCard: " + response.hasVCard());
+                    ctx.info("Response isError: " + response.isError());
+                    ctx.info("Response isSuccess: " + response.isSuccess());
+                    ctx.getSession().logout();
+                }
+            });
+        }
     };
     private VCardManager manager;
 
     @Override
     public void registerTests() {
-	add("Request vcard", requestVCard);
+        add("Request vcard", requestVCard);
     }
 
 }
