8ae8a4de8f6c9faaaa1e81e33f255edd40c98337
ashward.surevine <ashward.surevine@87f8d446-a148-0410-9e96-a52f46714620>
2010-02-22 13:45:14 +0000

    Tagged Hablar-0.2.1

---
+++ tags/Hablar-0.2.1/hablar/src/com/calclab/hablar/roster/client/groups/RosterGroupDisplay.java
@@ -0,0 +1,17 @@
+package com.calclab.hablar.roster.client.groups;
+
+import com.calclab.hablar.core.client.mvp.Display;
+
+public interface RosterGroupDisplay extends Display {
+
+    void add(RosterItemDisplay itemDisplay);
+
+    boolean isVisible();
+
+    RosterItemDisplay newRosterItemDisplay();
+
+    void remove(RosterItemDisplay itemDisplay);
+
+    void setVisible(boolean visible);
+
+}
---
+++ tags/Hablar-0.2.1/hablar/src/com/calclab/hablar/user/client/UserDisplay.java
@@ -0,0 +1,13 @@
+package com.calclab.hablar.user.client;
+
+import com.calclab.hablar.core.client.mvp.Display;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+
+public interface UserDisplay extends Display {
+
+    void addPage(EditorPage<?> page);
+
+    HasClickHandlers getCancel();
+
+    HasClickHandlers getUpdate();
+}
